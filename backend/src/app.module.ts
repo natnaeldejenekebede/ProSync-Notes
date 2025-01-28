08bb09fb0cb500a9a76c26e40ddcc78a208bfc6c
Natnael Dejene <natnaeldejene19@gmail.com>
2025-01-27 19:18:12 -0500

    Final: Update App Business Logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
+
 import { AuthModule } from "./auth/auth.module";
 import { NotesModule } from "./notes/notes.module";
 import { ProfileModule } from "./profile/profile.module";
@@ -7,9 +11,14 @@ import { SupabaseModule } from "./supabase/supabase.module";
 
 @Module({
   imports: [
-    // Loads .env
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      playground: true,
+      debug: true,
     }),
     AuthModule,
     NotesModule,
